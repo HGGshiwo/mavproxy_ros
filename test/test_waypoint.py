399e14900f7bf4d31b898d3ebb716f7817a3959f
HGGshiwo <3190102788@zju.edu.cn>
2026-04-24 11:03:53 +0800

    增加了返航异常行为的测试

--- test/test_waypoint.py
+++ test/test_waypoint.py
@@ -14,7 +14,7 @@ from mavproxy_ros.test.utils import gps_distance
 XY_THRESHOLD = 3  # 目标的距离阈值
 Z_THRESHOLD = 1.2  # 高度的距离阈值
 
-test_waypoint = [
+test_waypoint1 = [
     [120.14099425554586, 30.111834585498475, 10],
     [120.14089461794015, 30.111973425562567, 3],
     [120.1409098335351, 30.112108444153343, 15],
@@ -23,6 +23,12 @@ test_waypoint = [
     [120.1409432096789, 30.112290166996786, 6],
     [120.14080283354473, 30.11221289237057, 10],
     [120.1408582968425, 30.112159394516993, 7],
+]
+
+test_waypoint2 = [
+    [120.1408582968425, 30.112159394516993, 7],
+    [120.14080283354473, 30.11221289237057, 10],
+    [120.1409432096789, 30.112290166996786, 6],
 ]
 
 
@@ -35,10 +41,36 @@ class TestWaypoint(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_case1(self):
+    def set_waypoint(self, waypoint):
+        http_post("/set_waypoint", dict(waypoint=waypoint), check=True)
+
+        for i, wp in enumerate(waypoint[1:]):
+            for _ in range(10):
+                try:
+                    res = self.robot.ws_event_queue.get(timeout=50)
+                except:
+                    continue
+                if res.get("event", None) == "progress":
+                    break
+            else:
+                raise RuntimeError("没有出现进度信息!")
+
+            rospy.logerr(f"{i}, 收到消息!: {res}")
+            assert res.get("total", None) == len(waypoint) - 1, res
+            assert res.get("cur", None) == i + 1, f"{res}, {i}"
+            lat = self.robot.state["lat"]
+            lon = self.robot.state["lon"]
+            dist = gps_distance(lon, lat, wp[0], wp[1])
+            assert dist < XY_THRESHOLD, f"dist: {dist}, wp:[{wp[0]}, {wp[1]}]"
+            if self.robot.get_robot_type() == "drone":
+                dist_z = math.fabs(self.robot.state["rel_alt"] - wp[2])
+                assert dist_z < Z_THRESHOLD, f"z: {dist_z}"
+        self.robot.wait_for_state("state", "悬停状态")
+
+    def test_waypoint(self):
         """
-        1. 飞机起飞，悬停状态，飞一段航点，
-        2. 打开避障，再飞一段航点，返航
+        1. 飞机起飞，悬停状态，飞一段航点，降落
+        2. 从航点中起飞，返航（测试返航位置不是起飞点的问题）
         """
         IRIS_X = random.randint(-3, 3)
         IRIS_Y = random.randint(-3, 3)
@@ -49,31 +81,14 @@ class TestWaypoint(unittest.TestCase):
             http_post("/stop_pland", check=True)
             http_post("/stop_planner", check=True)
 
-            http_post("/set_waypoint", dict(waypoint=test_waypoint), check=True)
+            self.set_waypoint(test_waypoint1)
+            http_post("/land", check=True)
+            self.robot.wait_for_state("state", "地面状态", 120)
+
             home_lat = self.robot.state["lat"]
             home_lon = self.robot.state["lon"]
-            for i, wp in enumerate(test_waypoint[1:]):
-                for _ in range(10):
-                    try:
-                        res = self.robot.ws_event_queue.get(timeout=50)
-                    except:
-                        continue
-                    if res.get("event", None) == "progress":
-                        break
-                else:
-                    raise RuntimeError("没有出现进度信息!")
 
-                rospy.logerr(f"{i}, 收到消息!: {res}")
-                assert res.get("total", None) == len(test_waypoint) - 1, res
-                assert res.get("cur", None) == i + 1, f"{res}, {i}"
-                lat = self.robot.state["lat"]
-                lon = self.robot.state["lon"]
-                dist = gps_distance(lon, lat, wp[0], wp[1])
-                assert dist < XY_THRESHOLD, f"dist: {dist}, wp:[{wp[0]}, {wp[1]}]"
-                if self.robot.get_robot_type() == "drone":
-                    dist_z = math.fabs(self.robot.state["rel_alt"] - wp[2])
-                    assert dist_z < Z_THRESHOLD, f"z: {dist_z}"
-
+            self.set_waypoint(test_waypoint2)
             self.robot.wait_for_state("state", "悬停状态", 120)
             http_post("/return", check=True)
             self.robot.wait_for_state("state", "地面状态", 120)
